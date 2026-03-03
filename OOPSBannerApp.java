8a3b0a626060c31e465c7b2d48507d3439fade8c
Ashwathi <as6239@srmist.edu.in>
2026-03-03 12:24:24 +0530

    added UC8

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -117,7 +117,7 @@ public class OOPSBannerApp{
 	 System.out.println (oPattern[i] + " " +oPattern[i]+" "+pPattern[i]+" "+SPattern[i]);
         }
      }
-}*/
+}
 
 public class OOPSBannerApp{
  static class CharacterPatternMap {
@@ -164,4 +164,36 @@ public class OOPSBannerApp{
 		String message = "OOPS";
 		printMessage(message,charMaps);
 	}
+}*/
+
+
+import java.util.HashMap;
+public class OOPSBannerApp {
+public static HashMap<Character,String[]>createCharacterMap() {
+HashMap<Character,String[]>charMap = new HashMap<>();
+charMap.put('O',new String[] {"    ***   " ,"  *     * " ,"  *     * " ,"  *     * " ,"  *     * " ,"  *     * " ,"    ***   " });
+charMap.put('P',new String[] {"  ****** " ,"  *    * " ,"  *    * " ,"  ****** " ,"  *      " ,"  *      " ,"  *      " });
+charMap.put('S',new String[] {"  ***** " ,"  *     " ,"  *     " ,"  ***** " ,"      * " ,"      * " ,"  ***** " });
+return charMap;
 }
+public static void displayBanner(String message,HashMap<Character,String[]>charMap) {
+if (message == null || message.isEmpty()) return;
+int patternHeight = charMap.get('O').length;
+for(int line = 0; line < patternHeight;line++) {
+StringBuilder sb = new StringBuilder();
+for (char ch : message.toUpperCase().toCharArray()) {
+if (charMap.containsKey(ch)) {
+String[] pattern = charMap.get(ch);
+sb.append(pattern[line]).append(" ");
+}
+}
+System.out.println(sb.toString());
+}
+}
+public static void main (String[] args) {
+HashMap<Character,String[]>charMap=createCharacterMap();
+String message = "OOPS";
+displayBanner(message,charMap);
+}
+}
+
